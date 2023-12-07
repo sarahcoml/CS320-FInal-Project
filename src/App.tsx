14c1a1a33c4909b624cfeaa4b3ad9d4853b54a82
huda-abdulr <huda_abdulrasool@brown.edu>
2023-12-07 16:05:25 -0500

    feat: some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navigation from "./components/Navigation.tsx";
-import Main from "./components/Main.tsx"
+import Main from "./components/Main.tsx";
 const App = () => {
   return (
     <div id="page-container">
       <header>
-        <h1>CS320 Final Project</h1>
+        <Navigation />
+        <h1>Your Healthcare Inside + Out</h1>
       </header>
-      <Navigation />
       <Main />
     </div>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../styles/Navigation.css";
+import "../styles/Navigation.scss";
 const Navigation = () => (
   <nav>
     <NavLink className="link" to="/">
